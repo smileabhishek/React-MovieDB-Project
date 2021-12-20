5619419625d15d6bcd6f405d46cfa1c85e8d3596
Abhishek Anand <abhishekanand1138@gmail.com>
2021-12-20 22:26:59 +0530

    "Added Grid layout for popular movies"

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 //hooks
 import { useHomeFetch } from "../hooks/useHomeFetch";
 //image
@@ -21,6 +24,23 @@ const Home = () => {
                     text={state.results[0].overview}
                 />
             ) : null}
+            <Grid header="Popular Movies">
+                {state.results.map((movie) => (
+                    <Thumb
+                        key={movie.id}
+                        clickable
+                        image={
+                            movie.poster_path
+                                ? IMAGE_BASE_URL +
+                                  POSTER_SIZE +
+                                  movie.poster_path
+                                : NoImage
+                        }
+                        movieId={movie.id}
+                    />
+                ))}
+            </Grid>
+            <Spinner />
         </>
     );
 };
