f4f706c1caa33cb74591fb427f13101e001be99b
Abhishek Anand <abhishekanand1138@gmail.com>
2021-12-21 04:24:53 +0530

    "Added a seprate movie page  to render each movie when clicked"

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMBDLogo from "../../images/tmdb_logo.svg";
 
@@ -7,7 +8,9 @@ import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 const Header = () => (
     <Wrapper>
         <Content>
-            <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+            <Link to="/">
+                <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+            </Link>
             <TMDBLogoImg src={TMBDLogo} alt="tmbd-logo" />
         </Content>
     </Wrapper>
