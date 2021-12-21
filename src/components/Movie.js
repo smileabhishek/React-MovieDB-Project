8474c7ec0484e6974f6449ba9aa6795b1bee7a37
Abhishek Anand <abhishekanand1138@gmail.com>
2021-12-22 03:38:04 +0530

    "Added Actors component, base application complete"

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,10 +7,12 @@ import Grid from "./Grid";
 import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 //hooks
 import { useMovieFetch } from "../hooks/useMovieFetch";
 //Images
 import NoImage from "../images/no_image.jpg";
+import Actor from "./Actor";
 
 const Movie = () => {
     const { movieId } = useParams();
@@ -23,6 +25,25 @@ const Movie = () => {
         <>
             <BreadCrumb movieTitle={movie.original_title} />
             <MovieInfo movie={movie} />
+            <MovieInfoBar
+                time={movie.runtime}
+                budget={movie.budget}
+                revenue={movie.revenue}
+            />
+            <Grid header="Actors">
+                {movie.actors.map((actor) => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path
+                                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                                : NoImage
+                        }
+                    />
+                ))}
+            </Grid>
         </>
     );
 };
